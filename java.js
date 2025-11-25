32e4364e7f6d26dd835ef1cd2188c4fc83ba860c
camipro09 <49391020@ivt.edu.ar>
2025-11-25 08:52:05 -0300

    ff

--- java.js
+++ java.js
@@ -22,6 +22,50 @@ onValue(sensoresRef, (snapshot) => {
 });
 
 
-if (data <20 ) {
-    document.querySelector(msg-temp("el suelo esta muy seco"))
+function actualizarSemaforo(idSemaforo, color) {
+    const semaforo = document.getElementById(idSemaforo);
+    const luces = semaforo.querySelectorAll(".luz");
+
+    luces.forEach(luz => luz.classList.remove("encendida"));
+
+    if (color === "rojo")
+        luces[0].classList.add("encendida");
+    else if (color === "amarillo")
+        luces[1].classList.add("encendida");
+    else if (color === "verde")
+        luces[2].classList.add("encendida");
 }
+
+
+
+
+onValue(ref(db, "sensores/humedadSuelo"), (snapshot) => {
+    const valor = snapshot.val();
+    document.getElementById("msg-suelo").textContent = valor + "%";
+
+    if (valor < 30) actualizarSemaforo("semaforo-suelo", "rojo");
+    else if (valor < 60) actualizarSemaforo("semaforo-suelo", "amarillo");
+    else actualizarSemaforo("semaforo-suelo", "verde");
+});
+
+
+onValue(ref(db, "sensores/humedadAire"), (snapshot) => {
+    const valor = snapshot.val();
+    document.getElementById("msg-aire").textContent = valor + "%";
+
+    if (valor < 30) actualizarSemaforo("semaforo-aire", "rojo");
+    else if (valor < 60) actualizarSemaforo("semaforo-aire", "amarillo");
+    else actualizarSemaforo("semaforo-aire", "verde");
+});
+
+
+
+
+onValue(ref(db, "sensores/temperatura"), (snapshot) => {
+    const valor = snapshot.val();
+    document.getElementById("msg-temp").textContent = valor + "°C";
+
+    if (valor < 25) actualizarSemaforo("semaforo-temp", "verde");
+    else if (valor < 35) actualizarSemaforo("semaforo-temp", "amarillo");
+    else actualizarSemaforo("semaforo-temp", "rojo");
+});
